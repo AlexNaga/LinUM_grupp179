f489d7d4154762a6b89b5e05a016c597610f9837
Alex <alex.naga@hotmail.com>
2018-11-10 16:19:16 +0100

    Compute the power of P = U^2 / R

--- src/lib/libpower/libpower.c
+++ src/lib/libpower/libpower.c
@@ -1,16 +1,25 @@
+#include <math.h>
 #include <stdio.h>
 
 /**
  * Beräknar den totala effektutvecklingen med följande formell:
- * P = U * I (Spänning gånger strömmen)
+ * P = U^2 / R (Spänning i kvadrat delat på resistansen)
  */
 float calc_power_r(float volt, float resistance)
 {
+  float power = 0;
+
+  if (resistance > 0)
+  {
+    power = pow(volt, 2.0) / resistance;
+  }
+
+  return power;
 }
 
 /**
  * Beräknar den totala effektutvecklingen med följande formell:
- * P = U^2 / R (Spänning i kvadrat delat på resistansen)
+ * P = U * I (Spänning gånger strömmen)
  */
 float calc_power_i(float volt, float current)
 {

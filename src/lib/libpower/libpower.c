d5b0f8925d70b6d91780075dbd5692883aa4976f
Alex <alex.naga@hotmail.com>
2018-11-10 14:50:34 +0100

    Added comments

--- src/lib/libpower/libpower.c
+++ src/lib/libpower/libpower.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ * Beräknar den totala effektutvecklingen med följande formell:
+ * P = U * I (Spänning gånger strömmen)
+ */
 float calc_power_r(float volt, float resistance)
 {
 }
 
+/**
+ * Beräknar den totala effektutvecklingen med följande formell:
+ * P = U^2 / R (Spänning i kvadrat delat på resistansen)
+ */
 float calc_power_i(float volt, float current)
 {
 }
